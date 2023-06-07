32d39b69d523dd046c763b318e16990d7df88fbc
SophMe <116891325+SophMe@users.noreply.github.com>
2023-06-07 17:05:24 +0200

    add option to change bg color via TouchableOpacity

--- components/Chat.js
+++ components/Chat.js
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 const Chat = ({ route, navigation }) => {
-  const { name } = route.params;              // extracts value of 'name' parameter from route prop, 
+  const { name, color } = route.params;       // extracts value of 'name' and 'color' parameters from route prop, 
                                               // params is an object that holds the parameters or data passed to the component during navigation
   useEffect(() => {
     navigation.setOptions({ title: name });   // update navigation title to the value of the 'name' state variable when the component mounts
   }, []);                                     // empty dependency array [] ensures that it runs only once
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: color }]}>
       <Text>Hello Chat!</Text>
     </View>
   );
--- components/Start.js
+++ components/Start.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, Button, TextInput, ImageBackground, Image } from "react-native";
+import { StyleSheet, View, Text, Button, TextInput, ImageBackground, Image, TouchableOpacity } from "react-native";
 import appBackground from "../assets/image.png";
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState('');     // initialize state variable named 'name' and function named 'setName' with initial value of an empty string
+  const [color, setColor] = useState('');   // users can change background color with TouchableOpacity
+  const colorOption = [ '#d2b0fe', '#b0befe', '#fed1b0', '#bcfeb0' ]
 
   return (
     <View style={styles.container}>
@@ -20,9 +22,23 @@ const Start = ({ navigation }) => {
         onChangeText={setName}               // value of 'name' state changes according to text input from the user
         placeholder="Type your name here"
       />
+      <View>
+        <Text style={styles.text}>Choose a background color</Text>
+        <View style={styles.colorContainer}>
+          {colorOption.map((colorItem, index) => (
+            <TouchableOpacity
+              key={index}
+              style={[styles.colorOption, { backgroundColor: colorItem }]}
+              onPress={() => setColor(colorItem)}
+            />
+          ))}
+        </View>
+      </View>
+
       <Button
       title="Enter chat room"
-      onPress={() => navigation.navigate('Chat', { name: name })} // object as a second parameter representing the data we want to use in the screen we're transitioning to
+      onPress={() => navigation.navigate('Chat', { name: name, color: color })} // object as a second parameter representing the data we want to use in the screen we're transitioning to
+      color="whitesmoke"
       />
     </View>
   );
@@ -34,15 +50,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    textAlign: 'center',
+    padding: 5
+  },
   textInput: {
     width: '88%',
     padding: 15,
     borderWidth: 1,
     marginTop: 15,
-    marginBottom:15
+    marginBottom: 15
   },
   image: {
     position: 'absolute',
+  },
+  colorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  colorOption: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    margin: 5
   }
 });
 
