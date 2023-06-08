72d7800b33e5b3bde506fdb9e64dcfa0adbfd67b
SophMe <116891325+SophMe@users.noreply.github.com>
2023-06-08 14:55:23 +0200

    add Bubble, system message, quick reply

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,11 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const [messages, setMessages] = useState(); // the 'messages' state is used to store an array of chat messages, setMessages is a function
   const { name, color } = route.params;       // extracts value of 'name' and 'color' parameters from route prop, 
                                               // params is an object that holds the parameters or data passed to the component during navigation
-
-  const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages)) // 'previousMessages' accesses the previous state of 'messages'
-  };
 
   useEffect(() => {
     setMessages([
@@ -23,20 +19,70 @@ const Chat = ({ route, navigation }) => {
           avatar: "https://cataas.com/cat/says/hello%20world!"
         },
       },
+      {
+        _id: 2,
+        text: "This is a system message",
+        createdAt: new Date(),
+        system: true,
+      },
+      { // message with quick reply options
+        _id: 3,
+        text: "Gifted Chat is amazing. Do you agree?",
+        createdAt: new Date(),
+        quickReplies: {
+          type: "agree",
+          keepIt: true,
+          values: [
+            {
+              title: "Yes",
+              value: "yes",
+            },
+            {
+              title: "No",
+              value: "no",
+            },
+          ],
+          user: {
+            _id: 1,
+            name: "React Native",
+            avatar: "https://cataas.com/cat/says/hello%20world!"
+          },
+        }
+      }
     ]);
     navigation.setOptions({ title: name });   // update navigation title to the value of the 'name' state variable when the component mounts
   }, []);                                     // empty dependency array [] ensures that it runs only once, simulating the behavior of componentDidMount
 
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages)) // 'previousMessages' accesses the previous state of 'messages'
+  };
+
+  const renderBubble = (props) => {
+    return <Bubble 
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#a3e0cc"
+        },
+        left: {
+          backgroundColor: "#e0d4a3"
+        }
+      }}
+    />
+  }
+  
   return (
     <View style={[styles.container, { backgroundColor: color }]}>
       <GiftedChat
         messages={messages}
+        renderBubble={renderBubble}
         onSend={messages => onSend(messages)}
         user={{
           _id: 1
         }}
       />
-      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+      { Platform.OS === "android" ? <KeyboardAvoidingView behavior="height" /> : null }
     </View>
   )
 }
