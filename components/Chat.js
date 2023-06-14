19090147959729cddf874b4e7c32418a80633570
SophMe <116891325+SophMe@users.noreply.github.com>
2023-06-14 10:10:46 +0200

    correct useState, add KeyboardAvoidingView

--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
+import { StyleSheet, View, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 import { collection, onSnapshot, addDoc, query, orderBy  } from "firebase/firestore";
  
 const Chat = ({ route, navigation, db }) => {
-  const [messages, setMessages] = useState();       // the 'messages' state is used to store an array of chat messages, setMessages is a function
+  const [messages, setMessages] = useState([]);       // the 'messages' state is used to store an array of chat messages, setMessages is a function
   const { name, color, userID } = route.params;     // extracts value of 'name' and 'color' parameters from route prop, 
                                                     // params is an object that holds the parameters or data passed to the component during navigation
 
   useEffect(() => {
+    navigation.setOptions({ title: name });
     const unsubscribe = onSnapshot(
       query(collection(db, "messages"), orderBy("createdAt", "desc")),
       (snapshot) => {
@@ -21,12 +22,9 @@ const Chat = ({ route, navigation, db }) => {
             user: data.user,
           };
         });
-  
         setMessages(newMessages);
       }
     );
-  
-    navigation.setOptions({ title: name });
   
     return () => {
       unsubscribe();  // Clean the listener when the component unmounts
@@ -34,7 +32,7 @@ const Chat = ({ route, navigation, db }) => {
   }, []);             // empty dependency array [] ensures that it runs only once, simulating the behavior of componentDidMount
   
   const onSend = (newMessages) => {
-  addDoc(collection(db, "messages"), newMessages[0]);
+  addDoc(collection(db, "messages"), newMessages[0]); // send new messages to Firestore
   }
 
   const renderBubble = (props) => {
@@ -52,20 +50,22 @@ const Chat = ({ route, navigation, db }) => {
   }
   
   return (
-    <View style={[styles.container, { backgroundColor: color }]}>
-      <GiftedChat
-        messages={messages}
-        renderBubble={renderBubble}
-        onSend={messages => onSend(messages)}
-        user={{
-          _id: userID,
-          name: name
-        }}
-      />
-      { Platform.OS === "android" ? <KeyboardAvoidingView behavior="height" /> : null }
-    </View>
-  )
-}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={[styles.container, { backgroundColor: color }]}>
+        <GiftedChat
+          messages={messages}
+          renderBubble={renderBubble}
+          onSend={(messages) => onSend(messages)} // message or messages??
+          user={{
+            _id: String(userID),
+            name: name
+          }}
+        />
+        { Platform.OS === "android" ? <KeyboardAvoidingView behavior="height" /> : null }
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
